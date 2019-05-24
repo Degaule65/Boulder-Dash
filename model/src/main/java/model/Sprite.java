bd84758070c1d78c930d2b4062f308be48924797
BP <benoit.perriguet@viacesi.fr>
2019-05-24 16:47:16 +0200

    Sprite : more functionnality

--- model/src/main/java/model/Sprite.java
+++ model/src/main/java/model/Sprite.java
@@ -1,40 +1,38 @@
 package model;
 
-public class Sprite  {
+import java.awt.Image;
 
-	
-		
-		private char strImage;
-		private String imageName;
-		
-		public char getStrImage() {
-			return strImage;
-		}
-		
-		public void setStrImage(char strImage) {
-			this.strImage = strImage;
-		}
-		
-		
-		public String getImageName() {
-			return imageName;
-		}
-		
-		public void imageName(String imageName) {
-			this.imageName = imageName;
-		}
-		
-		
-		
-		
-		
-		
-		
+public class Sprite {
 
-		
-		
-		// TODO Auto-generated method stub
-
+	public Sprite(String name) {
+		loadImage(name);
 	}
 
+	private char strImage;
+	private String imageName;
+	private Image image;
 
+	public char getStrImage() {
+		return strImage;
+	}
+
+	public void setStrImage(char strImage) {
+		this.strImage = strImage;
+	}
+
+	public String getImageName() {
+		return imageName;
+	}
+
+	public void imageName(String imageName) {
+		this.imageName = imageName;
+	}
+	
+	public Image getImage() {
+		return image;
+	}
+
+	private void loadImage(String name) {
+		//name + Model.STYLE + ".jpg"
+	}
+}
