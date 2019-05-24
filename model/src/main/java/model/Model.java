569a689f5afb0a05bd01a7821c15ac2bb24301cc
Tangauche <44364688+Tangauche@users.noreply.github.com>
2019-05-24 11:38:56 +0200

    Mobile first changes

--- model/src/main/java/model/Model.java
+++ model/src/main/java/model/Model.java
@@ -9,77 +9,35 @@ import entity.HelloWorld;
 /**
  * The Class Model.
  *
- * @author Jean-Aymeric Diet
+ * @author [enter name]
  */
 public final class Model extends Observable implements IModel {
 
-	/** The helloWorld. */
-	private HelloWorld helloWorld;
 
-	/**
-	 * Instantiates a new model.
-	 */
+	private IHero hero;
+	public int width;
+	public int heigth;
+	public int STYLE;
+
+
 	public Model() {
-		this.helloWorld = new HelloWorld();
+		
 	}
 
-	/**
-     * Gets the hello world.
-     *
-     * @return the hello world
-     */
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see contract.IModel#getMessage()
-	 */
-	public HelloWorld getHelloWorld() {
-		return this.helloWorld;
+	public IHero getHero() {
+		return hero;
 	}
 
-	/**
-     * Sets the hello world.
-     *
-     * @param helloWorld
-     *            the new hello world
-     */
-	private void setHelloWorld(final HelloWorld helloWorld) {
-		this.helloWorld = helloWorld;
-		this.setChanged();
-		this.notifyObservers();
+	public void setHero(IHero hero) {
+		this.hero = hero;
 	}
-
-	/**
-     * Load hello world.
-     *
-     * @param code
-     *            the code
-     */
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see contract.IModel#getMessage(java.lang.String)
-	 */
-	public void loadHelloWorld(final String code) {
-		try {
-			final DAOHelloWorld daoHelloWorld = new DAOHelloWorld(DBConnection.getInstance().getConnection());
-			this.setHelloWorld(daoHelloWorld.find(code));
-		} catch (final SQLException e) {
-			e.printStackTrace();
-		}
+	
+	public IMap getMap() {
+		
 	}
-
-	/**
-     * Gets the observable.
-     *
-     * @return the observable
-     */
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see contract.IModel#getObservable()
-	 */
-	public Observable getObservable() {
-		return this;
+	public void setMap(IMap Map) {
+		
 	}
+	
+	
 }
