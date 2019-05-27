de066ec82966e289a8e2c905bf1ea17ea34b566e
Degaule65 <44675765+Degaule65@users.noreply.github.com>
2019-05-27 16:59:04 +0200

    hero(Sprite)

--- model/src/main/java/model/mobile/Hero.java
+++ model/src/main/java/model/mobile/Hero.java
@@ -1,29 +1,62 @@
 package model.mobile;
 
+import java.io.IOException;
+import contract.IHero;
+import model.IMap;
 import model.ObjectType;
 import model.Sprite;
 
-public class Hero extends Mobile {
-	
-	private static final Sprite sprite = new Sprite('R', "Hero.gif");
+public class Hero extends Mobile implements IHero {
 
-	public Hero() {
+	private static final Sprite sprite = new Sprite('H', "main/images/droite/Hero.gif");
+	private static final Sprite spriteDown = new Sprite('H', "main/images/droite/HeroDown.gif");
+	private static final Sprite spriteTurnLeft = new Sprite('H', "main/images/droite/HeroLeft.gif");
+	private static final Sprite spriteTurnRight = new Sprite('H', "main/images/droite/HeroRight.gif");
+	private static final Sprite spriteUP = new Sprite('H', "main/images/droite/HeroRight.gif");
+
+	public Hero(final int x, final int y, final IMap map) throws IOException {
 		super(sprite, ObjectType.HERO);
+		spriteTurnLeft.loadImage();
+		spriteTurnRight.loadImage();
+		spriteUP.loadImage();
 	}
+	
 
 	public void moveUp() {
 
+		this.setSprite(spriteUP);
+		this.setY(+1);
 	}
 
 	public void moveDown() {
+
+		this.setSprite(spriteDown);
+		this.setY(+1);
 
 	}
 
 	public void moveRight() {
 
+		this.setSprite(spriteTurnRight);
+		this.setX(+1);
+
 	}
 
 	public void moveLeft() {
 
+		this.setSprite(spriteTurnLeft);
+		this.setX(-1);
+
 	}
+
+	public void none() {
+
+		this.setSprite(sprite);
+	}
+	
+	public Hero(Sprite sprite, ObjectType objectType) {
+		super(sprite, objectType);
+	}
+
+	
 }
