cb1261731c704fde1da235dd8f0ccb8236a9a261
BP <benoit.perriguet@viacesi.fr>
2019-05-27 14:23:23 +0200

    Factory update

--- model/src/main/java/model/motionless/Background.java
+++ model/src/main/java/model/motionless/Background.java
@@ -5,12 +5,9 @@ import model.Sprite;
 
 public class Background extends MotionlessElement {
 
-@SuppressWarnings("unused")
-private static final Sprite SPRITE = new Sprite(' ', "/image/wall.jpg");
+	private static final Sprite SPRITE = new Sprite(' ', "/image/wall.jpg");
 
-	Background(Sprite sprite) {
-		
-		super(sprite,ObjectType.BACKGROUND); 
-
-}
+	public Background() {
+		super(SPRITE, ObjectType.BACKGROUND);
+	}
 }
--- model/src/main/java/model/motionless/Factory.java
+++ model/src/main/java/model/motionless/Factory.java
@@ -1,6 +1,12 @@
 package model.motionless;
 
 public abstract class Factory {
+	
+	private static final Background background = new Background();
+	private static final Dirt dirt = new Dirt();
+	private static final Wall wall = new Wall();
+	
+	private static MotionlessElement[] motionlessElements = {background, dirt, wall};
 
 	public static MotionlessElement createBackground() {
 		return null;
@@ -12,6 +18,15 @@ public abstract class Factory {
 	
 	public static MotionlessElement createWalle() {
 		return null;
+	}
+	
+	public static MotionlessElement getFromFileSymbole(final char symbol) {
+		for (final MotionlessElement motionlessElement : motionlessElements) {
+			if (motionlessElement.getSprite().getStrImage() == symbol) {
+				return motionlessElement;
+			}
+		}
+		return background;
 	}
 
 }
