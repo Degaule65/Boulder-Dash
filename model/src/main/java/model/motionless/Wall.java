6946d08a694408a2c31b67b6bf79ca05b78e58bc
Degaule65 <44675765+Degaule65@users.noreply.github.com>
2019-05-27 10:32:45 +0200

    test_pour_le_wall

--- model/src/main/java/model/motionless/Wall.java
+++ model/src/main/java/model/motionless/Wall.java
@@ -1,9 +1,20 @@
 package model.motionless;
 
-public class Wall {
+import model.ObjectType;
+import model.Sprite;
 
-	Wall() {
+public class Wall extends MotionlessElement{
 
+	Wall(Sprite sprite) {
+		super(sprite);
+		// TODO Auto-generated constructor stub
 	}
+
+	// private static final Sprite SPRITE = new Sprite(' ', "/image/wall.jpg");
+
+	    /**
+	     * Instantiates a new ditchLeft.
+	     */
+	       // super(SPRITE, ObjectType.DIRT);
 
 }
