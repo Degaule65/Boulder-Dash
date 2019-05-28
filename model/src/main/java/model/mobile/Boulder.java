e9e064832ecb159230a2e31740f844fad0d35027
Degaule65 <44675765+Degaule65@users.noreply.github.com>
2019-05-28 14:40:28 +0200

    boulder

--- model/src/main/java/model/mobile/Boulder.java
+++ model/src/main/java/model/mobile/Boulder.java
@@ -9,9 +9,11 @@ public class Boulder extends NonHeroMobile {
 
 	private static final Sprite SPRITE = new Sprite('R', "main/images/pierre/ROCK.gif");
 	
-	public Boulder(Sprite sprite, ObjectType objectType) throws IOException {
+	public Boulder(Sprite sprite, ObjectType objectType, final int x, final int y) throws IOException {
 		super(SPRITE, ObjectType.BOULDER);
 		sprite.loadImage();
+		this.setX(x);
+		this.setY(y);
 	}
 
 }
