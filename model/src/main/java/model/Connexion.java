fbcf93842c62513be9c46dd53cad9a5913172e98
AC <alexis.canteli@viacesi.fr>
2019-05-29 09:42:52 +0200

    Update db connection + sql statement

--- model/src/main/java/model/Connexion.java
+++ model/src/main/java/model/Connexion.java
@@ -2,14 +2,36 @@ package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Connexion {
 
 	public static void main(String[] args) {
-		connecterDB();
-	}
+		// connecterDB();
+		// }
 
-	Connection cnx = connecterDB();
+		try
+
+		{
+
+			Connection connection = connecterDB();
+			Statement statement;
+			ResultSet resultset;
+
+			statement = connection.createStatement();
+			resultset = statement.executeQuery("SELECT * FROM map");
+
+			while (resultset.next()) {
+				System.out.println(resultset.getString("Content"));
+				System.out.println(resultset.getInt("IDMap"));
+				System.out.println();
+			}
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
 
 	public static Connection connecterDB() {
 		try {
@@ -18,9 +40,9 @@ public class Connexion {
 			String url = "jdbc:mysql://localhost:3306/jpublankproject?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=false";
 			String user = "root";
 			String password = "";
-			Connection cnx = DriverManager.getConnection(url, user, password);
+			Connection connection = DriverManager.getConnection(url, user, password);
 			System.out.println("You are now connected to the DataBase");
-			return cnx;
+			return connection;
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
