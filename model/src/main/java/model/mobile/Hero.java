9a425ff8a49a70c7ed6adcdbeb6e61bc19735ffb
BP <benoit.perriguet@viacesi.fr>
2019-06-01 16:21:31 +0200

    Change sprite based on case

--- model/src/main/java/model/mobile/Hero.java
+++ model/src/main/java/model/mobile/Hero.java
@@ -1,5 +1,6 @@
 package model.mobile;
 
+import contract.ControllerOrder;
 import contract.IElement;
 import contract.IHero;
 import contract.ObjectType;
@@ -7,8 +8,6 @@ import contract.Sprite;
 import model.IMap;
 
 public class Hero extends Mobile implements IHero {
-	
-	
 
 	private final static String imageUp = "images/Hero/heroUp.gif";
 	private final static String imageDown = "images/Hero/heroDown.gif";
@@ -21,7 +20,7 @@ public class Hero extends Mobile implements IHero {
 	private static final Sprite spriteDown = new Sprite(Hero.fileSym, imageDown);
 	private static final Sprite spriteTurnLeft = new Sprite(Hero.fileSym, imageLeft);
 	private static final Sprite spriteTurnRight = new Sprite(Hero.fileSym, imageRight);
-	private static final Sprite spriteUP = new Sprite(Hero.fileSym, imageUp);
+	private static final Sprite spriteUp = new Sprite(Hero.fileSym, imageUp);
 
 	public Hero(final int x, final int y, final IMap map) {
 		super(sprite, ObjectType.HERO, x, y, map);
@@ -31,15 +30,15 @@ public class Hero extends Mobile implements IHero {
 		IElement element = this.getMap().getOnTheMapXY(this.getX(), this.getY() - 1);
 
 		if (element.getObjectType() == ObjectType.DIAMOND) {
-			this.setSprite(spriteUP);
+			this.setSprite(spriteUp);
 			this.setY(this.getY() + 1);
 			this.setHasMoved();
 		} else if (element.getObjectType() == ObjectType.BOULDER) {
-			this.setSprite(spriteUP);
+			this.setSprite(spriteUp);
 		} else if (element.getObjectType() == ObjectType.WALL) {
-			this.setSprite(spriteUP);
+			this.setSprite(spriteUp);
 		} else {
-			this.setSprite(spriteUP);
+			this.setSprite(spriteUp);
 			this.setY(this.getY() - 1);
 			this.setHasMoved();
 		}
@@ -47,10 +46,10 @@ public class Hero extends Mobile implements IHero {
 
 	public void moveDown() {
 		IElement element = this.getMap().getOnTheMapXY(this.getX(), this.getY() + 1);
-		
+
 		if (element.getObjectType() == ObjectType.DIAMOND) {
 			this.setSprite(spriteDown);
-			Diamond.setVisible(false); 
+			Diamond.setVisible(false);
 		} else if (element.getObjectType() == ObjectType.BOULDER) {
 			this.setSprite(spriteDown);
 		} else if (element.getObjectType() == ObjectType.WALL) {
@@ -65,10 +64,10 @@ public class Hero extends Mobile implements IHero {
 
 	public void moveRight() {
 		IElement element = this.getMap().getOnTheMapXY(this.getX() + 1, this.getY());
-		
+
 		if (element.getObjectType() == ObjectType.DIAMOND) {
 			this.setSprite(spriteTurnRight);
-			Diamond.setVisible(false); 
+			Diamond.setVisible(false);
 		} else if (element.getObjectType() == ObjectType.BOULDER) {
 			this.setSprite(spriteTurnRight);
 		} else if (element.getObjectType() == ObjectType.WALL) {
@@ -82,10 +81,10 @@ public class Hero extends Mobile implements IHero {
 
 	public void moveLeft() {
 		IElement element = this.getMap().getOnTheMapXY(this.getX() - 1, this.getY());
-		
+
 		if (element.getObjectType() == ObjectType.DIAMOND) {
 			this.setSprite(spriteTurnLeft);
-			
+
 		} else if (element.getObjectType() == ObjectType.BOULDER) {
 			this.setSprite(spriteTurnLeft);
 		} else if (element.getObjectType() == ObjectType.WALL) {
@@ -96,5 +95,24 @@ public class Hero extends Mobile implements IHero {
 			this.setHasMoved();
 		}
 
+	}
+
+	public void changeSprite(ControllerOrder order) {
+		switch (order) {
+		case UP:
+			this.setSprite(spriteUp);
+			break;
+		case DOWN:
+			this.setSprite(spriteDown);
+			break;
+		case LEFT:
+			this.setSprite(spriteTurnLeft);
+			break;
+		case RIGHT:
+			this.setSprite(spriteTurnRight);
+			break;
+		default:
+			break;
+		}
 	}
 }
