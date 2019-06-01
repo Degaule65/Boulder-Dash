fe3715c2b0c6cb4c7fc8a0343d85955302878684
Tangauche <44364688+Tangauche@users.noreply.github.com>
2019-06-01 17:28:22 +0200

    Gravity method changed

--- model/src/main/java/model/mobile/NonHeroMobile.java
+++ model/src/main/java/model/mobile/NonHeroMobile.java
@@ -1,5 +1,6 @@
 package model.mobile;
 
+import contract.IElement;
 import contract.ObjectType;
 import contract.Sprite;
 import model.IMap;
@@ -21,14 +22,46 @@ public class NonHeroMobile extends Mobile {
 	}
 
 	public void moveDown() {
+		this.setY(this.getY() + 1);
 		this.setHasMoved();
 	}
 
-	public void moveDownLeft() {
+	public void moveLeft() {
+		this.setX(this.getX() - 1);
 		this.setHasMoved();
 	}
 
-	public void moveDownRight() {
+	public void moveRight() {
+		this.setX(this.getX() + 1);
 		this.setHasMoved();
+	}
+	
+	public void gravity() {
+		IElement elementdown = this.getMap().getOnTheMapXY(this.getX(), this.getY() + 1);
+		IElement elementdownright = this.getMap().getOnTheMapXY(this.getX() - 1, this.getY() + 1);
+		IElement elementdownleft = this.getMap().getOnTheMapXY(this.getX() + 1, this.getY() + 1);
+		IElement elementleft = this.getMap().getOnTheMapXY(this.getX() + 1, this.getY());
+		IElement elementright = this.getMap().getOnTheMapXY(this.getX() - 1, this.getY());
+		
+		if (elementdown.getObjectType() == ObjectType.DIAMOND || 
+				elementdown.getObjectType() == ObjectType.BOULDER || 
+				elementdown.getObjectType() == ObjectType.WALL)
+		{  isFalling = true;
+			if (elementdownright.getObjectType() == ObjectType.BACKGROUND && 
+				elementright.getObjectType() == ObjectType.BACKGROUND) {
+				moveRight();
+				isFalling = false;
+				
+			} else if (elementdownleft.getObjectType() == ObjectType.BACKGROUND && 
+				elementleft.getObjectType() == ObjectType.BACKGROUND) {
+				moveLeft();
+				isFalling = false;
+			}
+		} else if(elementdown.getObjectType() == ObjectType.BACKGROUND) {
+			moveDown();
+		}
 	}
 }
+
+
+
