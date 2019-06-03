61f325808e14d16461deba0b3f1a1eb10fe2d799
BP <benoit.perriguet@viacesi.fr>
2019-06-03 13:59:01 +0200

    Test correction

--- model/src/test/java/model/AbstractFactoryTest.java
+++ model/src/test/java/model/AbstractFactoryTest.java
@@ -1,43 +1,23 @@
 package model;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import model.mobile.MobileFactory;
+import model.motionless.MotionlessFactory;
 
 public class AbstractFactoryTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
 	@Test
 	public void testGetFactory() {
-			IFactory factory = AbstractFactory.getFactory('H');
-			assertTrue(factory instanceof MobileFactory);
-	}
-	
-	@Test
-	public void testGetFactory2() {
-			IFactory factory = AbstractFactory.getFactory('H');
-			assertNotNull(factory);
+		IFactory factory = AbstractFactory.getFactory('H');
+		assertTrue(factory instanceof MobileFactory);
+		assertNotNull(factory);
+		factory = AbstractFactory.getFactory('=');
+		assertTrue(factory instanceof MotionlessFactory);
+		assertNotNull(factory);
 	}
 
 }
