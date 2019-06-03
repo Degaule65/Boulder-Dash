b10f0631e766a8851f5cdf7140b60057390f3b42
BP <benoit.perriguet@viacesi.fr>
2019-06-03 11:07:10 +0200

    Working DB access

--- model/src/main/java/model/DBCall.java
+++ model/src/main/java/model/DBCall.java
@@ -1,49 +1,34 @@
 package model;
 
-import java.sql.*;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-
-/**
- *
- * 
- */
 public class DBCall {
 
 	/**
 	 * Get skills by candidate id
+	 * @throws SQLException 
 	 *
 	 * 
 	 */
-	public static void test(int IDmap) {
-		
-		//
+	public Map getMap(int IDmap) throws SQLException {
 		String query = "{ call test(?) }";
-		ResultSet rs;
-		String url = "jdbc:mysql://localhost:3306/jpublankproject?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=false";
-		String user = "root";
-		String password = "";
-		try (Connection conn = DriverManager.getConnection(url, user, password);
-
-				CallableStatement stmt = conn.prepareCall(query)) {
-
-			stmt.setInt(IDmap, 1);
-
-			rs = stmt.executeQuery();
-			if (rs.first()) {
-				System.out.println(String.format("Style " + rs.getString("style") +"\nContent "+ rs.getString("content")+ " ") + "\nwidth " + rs.getString("width") + "\nheight " + rs.getString("height"));
-			}
-					
-
-		} catch (SQLException ex) {
-			System.out.println(ex.getMessage());
-		}
-	}
-
-	/**
-	 *
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		test(1);
+		Connection conn = DBConnection.getInstance().getConnection();
+		CallableStatement stmt;
+		int style;
+		int width;
+		int heigth;
+		String content;
+		stmt = conn.prepareCall(query);
+		stmt.setInt(IDmap, 1);
+		ResultSet rs = stmt.executeQuery();
+		rs.first();
+		style = rs.getInt("style");
+		width = rs.getInt("width");
+		heigth = rs.getInt("height");
+		content = rs.getString("content");
+		return new Map(content, width, heigth, style);
 	}
 }
